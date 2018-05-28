9adb3b14f4516eb6a4edd57d74028acfcc7d3b60
AnderskdKnudsen <anderskdknudsen@gmail.com>
2018-05-28 16:44:33 +0200

    lol

--- app.js
+++ app.js
@@ -6,16 +6,21 @@ const expressSession = require('express-session');
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
+var startPage = {
+    index: "login.html"
+};
+
 app.set('port', process.env.PORT || 3000);
-app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use("/", express.static("public", startPage));
+
 
 var server = app.listen(app.get('port'), err => {
-    if(err) console.log('Couldn\'t connect on port', app.get('port') + ". Error:", err.stack);
+    if (err) console.log('Couldn\'t connect on port', app.get('port') + ". Error:", err.stack);
     else console.log('Connected on port', app.get('port'));
 });
 
 app.get('/', (req, res) => {
-    res.sendFile('index.html');
+    res.sendFile('login.html');
 });
