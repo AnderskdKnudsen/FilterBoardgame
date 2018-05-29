d9c94e3cdb38f99844b20babd7fcc1b373d91c28
AnderskdKnudsen <anderskdknudsen@gmail.com>
2018-05-29 17:19:00 +0200

    ladga

--- mongo/mongoUser.js
+++ mongo/mongoUser.js
@@ -2,25 +2,6 @@ const mongo = require('mongodb').MongoClient;
 
 let path = 'mongodb://localhost:27017/FilterBoardgame';
 
-// function insert(user) {
-//     mongo.connect(path, (err, db) => {
-//         if (err) {
-//             console.log('Error connecting to the database', err.stack);
-//             return;
-//         }
-//         let collection = db.collection('users');
-
-//         collection.insertOne(user, (err, result) => {
-//             if (err) console.log('Error inserting in database', err.stack);
-//             else {
-//                 console.log('inserted: ', user);
-//                 db.close();
-//                 return user;
-//             }
-//         });
-//     });
-// }
-
 function insert(user) {
     return new Promise((resolve, reject) => {
         mongo.connect(path, (err, db) => {
