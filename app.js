a7fa76efde5c223ee809b4f1386d72fd4876a6ed
AnderskdKnudsen <anderskdknudsen@gmail.com>
2018-06-03 16:23:32 +0200

    hej

--- app.js
+++ app.js
@@ -19,13 +19,6 @@ app.use(expressSession({
     saveUninitialized: false
 }));
 
-
-/*
-Minimum players, maximum players and genre should be dropdown
-*/
-
-
-
 var server = app.listen(app.get('port'), err => {
     if (err) console.log('Couldn\'t connect on port', app.get('port') + ". Error:", err.stack);
     else console.log('Connected on port', app.get('port'));
